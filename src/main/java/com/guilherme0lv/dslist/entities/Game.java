213c4dd49e19482dd487f1b5748ad129fce37926
Guilherme0lv <l>
2025-01-21 16:57:05 -0300

    properties, Game, ORM

---
+++ src/main/java/com/guilherme0lv/dslist/entities/Game.java
@@ -0,0 +1,122 @@
+package com.guilherme0lv.dslist.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_game")
+public class Game {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String titulo;
+	private Integer ano;
+	private String genero;
+	private String plataforma;
+	private String imgURL;
+	private String descricaoCurta;
+	private String descricao;
+	
+	public Game() {
+	}
+
+	public Game(Long id, String titulo, Integer ano, String genero, String plataforma, String imgURL, String descricaoCurta, String descricao) {
+		this.id = id;
+		this.titulo = titulo;
+		this.ano = ano;
+		this.genero = genero;
+		this.plataforma = plataforma;
+		this.imgURL = imgURL;
+		this.descricaoCurta = descricaoCurta;
+		this.descricao = descricao;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public Integer getAno() {
+		return ano;
+	}
+
+	public void setAno(Integer ano) {
+		this.ano = ano;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public String getPlataforma() {
+		return plataforma;
+	}
+
+	public void setPlataforma(String plataforma) {
+		this.plataforma = plataforma;
+	}
+
+	public String getImgURL() {
+		return imgURL;
+	}
+
+	public void setImgURL(String imgURL) {
+		this.imgURL = imgURL;
+	}
+
+	public String getDescricaoCurta() {
+		return descricaoCurta;
+	}
+
+	public void setDescricaoCurta(String descricaoCurta) {
+		this.descricaoCurta = descricaoCurta;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Game other = (Game) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+}
